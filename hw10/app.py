f56d3f3a5fbe8ab5e54dd6612234248f61c0cafd
Nellie <nellie.spektor@gmail.com>
2016-04-19 11:10:35 -0400

    h10

--- hw10/app.py
+++ hw10/app.py
@@ -1,36 +1,72 @@
-from flask import Flask, render_template, request, jsonify
-import random, json, re
-from math import radians, cos, sin, asin, sqrt
+import urllib2,json
+from flask import Flask, render_template, session, request, redirect, url_for
 
 app = Flask(__name__)
-global cities
 
-@app.route("/")
-def index():
-    return render_template("index.html");
+def timing(fn):
+    def inner(*args):
+        start = time.time()
+        result = fn(*args)
+        end = time.time()
+        print "run time: "+str((end-start)*1000.0)
+        return result
+    return inner
+def name(fn):
+    def inner2(*args):
+        print(fn.func_name)
+        return fn(*args)
+    return inner2
 
-@app.route("/distance")
-def distance():
-    lat1 = radians(float(request.args.get("lat1").encode('ascii', 'ignore')))
-    lon1 = radians(float(request.args.get("lon1").encode('ascii', 'ignore')))
-    lat2 = radians(float(request.args.get("lat2").encode('ascii', 'ignore')))
-    lon2 = radians(float(request.args.get("lon2").encode('ascii', 'ignore')))
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a))
-    r = 6371
-    return str(c * r)
 
-@app.route("/getCoordinates")
-def getPlace():
-    coords = cities[random.randint(0,len(cities)-1)].split('\t')
-    c = {'lat':coords[0],'lng':coords[1]}
-    return jsonify(c)
+@app.route("/",methods=["GET","POST"])
+@app.route("/home",methods=["GET","POST"])
+@timing
+@name
+def home():
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
+        w = request.form.get('word')
+        return redirect(url_for("answer",word=w))
 
-if __name__ == "__main__":
-    f = open('cities15000.txt', 'r')
-    cities = f.read().split('\n')
-    f.close()
+
+@app.route("/answer",methods=["GET","POST"])
+@app.route("/answer/<word>",methods=["GET","POST"])
+@timing
+@name
+def answer(word):
+    """ 
+    gets word that user entered in home page and uses etymology api to 
+    display its etymology
+    """
+    key="a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
+    url="http://api.wordnik.com:80/v4/word.json/%s/etymologies?useCanonical=true&api_key=%s"
+    #print "\n\n\n\nWAKAKA " + word + " AKAKAW\n\n\n\n"
+    try:
+        url = url%(word,key)
+        request = urllib2.urlopen(url)
+        result = request.read()
+        r = json.loads(result)
+        x = r[0][38:]
+        print x
+        x = x.replace("<ety>","")
+        x = x.replace("</ety>","")
+        x = x.replace("<ets>","")
+        x = x.replace("</ets>","")
+        x = x.replace("[","")
+        x = x.replace("]","")
+        x = x.replace("<er>","")
+        x = x.replace("</er>","")
+        x = x.replace("fr.","//from")
+        x = x.replace("cf.","//-//cf.")
+        x = x.replace("Cf.","//-//cf.")
+        d = x.split("//")
+        print str(d)
+        return render_template("answer.html",word=word, etymology=d, error=False)
+    except:
+        return render_template("answer.html",error=True, word="bad word", etymology=["no etymology for that word, try again"])
+
+
+if __name__=="__main__":
     app.debug = True
-    app.run(host = "0.0.0.0", port=8000)
+    app.run(host='0.0.0.0',port=8000)
