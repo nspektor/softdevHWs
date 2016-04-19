4378fe00fec42f53af9edaee23b8b193f06ae727
Nellie <nellie.spektor@gmail.com>
2016-04-19 10:17:46 -0400

    adder

--- random/jsClosure/foo.js
+++ random/jsClosure/foo.js
@@ -1,5 +1,33 @@
-zvar base = "this is in f1 in the global namespace";
+var base = "this is in f1 in the global namespace";
 var x = "x is in the global namespace";
 var f2 = function(){
     console.log("this is f2");
 };
+
+var f1 = {
+    x : "something in the f1 namespace",
+    f2 : f2,
+    f : function() {
+	console.log('this is in f in the f1 namespace');
+	console.log('x is ' + x);
+	console.log('f1.x is' + f1.x);
+	console.log('better way: ' + this.x);
+    }
+};
+var makeIncrementer = function(x){
+    var x = 0;
+    return function() {
+	x = x+1;
+	return x;
+    }
+};
+
+var makeAdder = function(n) {
+    var y = 0;
+    return function(){
+	y = y+n;
+	return y;
+    }
+
+};
+    
